52f754e5d204085eb9fdaa6d4b15bb3edcb4ff7a
kozuedoingregression <91571529+kozuedoingregression@users.noreply.github.com>
2025-03-16 14:58:36 +0530

    added google adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,13 +9,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   const pathname = usePathname(); // Get the current route
 
   // Define paths where the sidebar should be hidden
-  const hiddenPaths = ["/", "/telemetry","/signup"];
+  const hiddenPaths = ["/", "/telemetry", "/signup"];
 
   return (
     <ClerkProvider>
       <html lang="en">
         <head>
-          <link rel="manifest" href="/manifest.json"/>
+          <link rel="manifest" href="/manifest.json" />
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4218964130174968"
+            crossOrigin="anonymous"></script>
         </head>
         <body className="flex">
           {/* Hide sidebar if pathname matches any hidden path */}
